fd180184a8c5f78ace5cd2abb823143e7491aa73
Tynor Fujimoto <tynor@hey.com>
2021-07-11 16:20:59 -0700

    Remove unused common dir

--- src/common/error.ts
+++
@@ -1,8 +0,0 @@
-export class PrecisInvalidCharacterError extends Error {
-  public readonly index: number;
-
-  public constructor(index: number) {
-    super(`Invalid character at index ${index}`);
-    this.index = index;
-  }
-}
